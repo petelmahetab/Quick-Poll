a60056096d3bb1e71627e2a1595fd2d46d08c382
Mahetab Patel <patelmahetab9020@gmail.com>
2025-03-07 01:19:52 +0530

    Resolved

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-require("dotenv").config(); // Only needed locally; Vercel uses its own env vars
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
@@ -8,24 +8,23 @@ const connectDB = require("./config/db");
 
 const app = express();
 
-// Middleware to handle CORS
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173", // Set CLIENT_URL in Vercel
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
-
+app.get('/', (req, res) => {
+  res.send('Quick Poll Backend is running!');
+});
 app.use(express.json());
-
 
 connectDB();
 
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/poll", pollRoutes);
 
-
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-module.exports = app;
+module.exports = app; 
